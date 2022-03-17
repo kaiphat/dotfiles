317589541b9106695995700cd3da1b16e70520d0
Iliyapunko <53742299+Iliyapunko@users.noreply.github.com>
2022-03-17 15:14:31 +0300

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,16 @@
+# Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 New-Alias -Name n -Value nvim
 New-Alias -Name d -Value docker
 New-Alias -Name dc -Value docker-compose
 New-Alias -Name g -Value git
 
 Set-Location D:\Work
-Import-Module oh-my-posh
+# install scoop https://scoop.sh/
+# https://ohmyposh.dev/docs/prompt
+# scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
+oh-my-posh prompt init pwsh --config https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/v$(oh-my-posh --version)/themes/jandedobbeleer.omp.json | Invoke-Expression
+# Install-Module posh-git -Scope CurrentUser
 Import-Module posh-git
-Set-PoshPrompt -Theme ~/.posh-theme.json
+
 
 
